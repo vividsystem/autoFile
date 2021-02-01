357ae0bb6383fd63d471684ee1021d664b728593
vividsystem <notrealbutnotobot@gmail.com>
2021-02-01 14:20:05 +0100

    minor changes again

--- autofile.py
+++ autofile.py
@@ -16,10 +16,12 @@ def handler():
   #get_plugins(args)
   load_config(parsedconfig)
   run()
+  
 def run():
   while True:
     mover(parsedconfig)
 def debug_run():
   return
+
 if __name__ == "__main__":
   handler()
