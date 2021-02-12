aa2d9e13eba319b2c9f4df208da5281647f0c00b
vividsystem <notrealbutnotobot@gmail.com>
2021-02-12 08:04:09 +0100

    arg handler

--- autofile.py
+++ autofile.py
@@ -12,14 +12,18 @@ plugins = {}
 args = {}
 homedir = getenv("HOME")
 #repo_dir = getenv("HOME")+"/"+
-repo_dir = getcwd()+'/'+__file__ if __file__.startswith(homedir) == False else __file__
+repo_dir = getcwd()+'/'+__file__.strip("autofile.py") if __file__.startswith(homedir) == False else __file__.strip("autofile.py")
 def main():
-  load_config(parsedconfig)
-  while True:
-    mover(parsedconfig)
-
+  for index_number in range(len(argv)):
+    if argv[index_number] == "-m":
+      if index_number+1 <= len(argv)-1:
+        module = argv[index_number+1]+".py"
+        if module in listdir(repo_dir+"plugins/") and module != "__init__.py" and module != "__pycache__":
+          print("Module found!")
+      else:
+        raise Exception("No Value specified for -r")
 def debug_run():
   return
 
-#if __name__ == "__main__":
-  #main()
+if __name__ == "__main__":
+  main()
