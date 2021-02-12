72ad726eaae6cc52df96fd35a60da182b4cd1a24
vividsystem <notrealbutnotobot@gmail.com>
2021-02-12 11:07:58 +0100

    minor changes

--- autofile.py
+++ autofile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from sys import argv
-from os import listdir, getenv, getcwd
+from os import listdir, getenv, getcwd, system
 from utils import load_config, mover, load_specific_plugin
 parsedconfig = {
   "origins": [],
@@ -20,8 +20,11 @@ def main():
         print(repo_dir+"plugins/")
         plugin = load_specific_plugin(plugin_name,repo_dir+"plugins/")
         plugin.main()
-      else:
-        raise Exception("No Value specified for -r")
+    else:
+        raise Exception("No Value specified for -m")
+  if "update" in argv:
+    system("git pull")
+      
     
 def debug_run():
   return
